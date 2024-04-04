363ad15ba0f143c9487e4144c0df952d4dc5c341
Valeria Rodriguez <145387992+ValeriaNaige@users.noreply.github.com>
2024-04-03 23:29:21 -0600

    Practica 8
    Practica8: Action Sheet y Sliding Options

--- miApp-2132985v2/src/app/lista-alumnos/lista-alumnos.component.ts
+++ miApp-2132985v2/src/app/lista-alumnos/lista-alumnos.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-lista-alumnos',
   templateUrl: './lista-alumnos.component.html',
   styleUrls: ['./lista-alumnos.component.scss'],
 })
-export class ListaAlumnosComponent  implements OnInit {
+export class ListaAlumnosComponent{
+  alumnos: any = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"];
+  //constructor() { }
+  isActionSheetOpen = false;
+  public actionSheetButtons = [
+    {
+      text: 'Eliminar',
+      role: 'destructive',
+      data: {
+        action: 'delete',
+      },
+    },
+    {
+      text: 'Cancelar',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
 
-  constructor() { }
-
-  ngOnInit() {}
-
-  alumnos : any = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"];
-
+  setOpen(isOpen: boolean) {
+    this.isActionSheetOpen = isOpen;
+  }
 }
